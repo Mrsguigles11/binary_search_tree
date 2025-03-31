eeb5539a29c5fa266abd4cac76438e11d14497ea
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2025-03-31 19:40:21 +0100

    Create a working insert method.

--- script.js
+++ script.js
@@ -1,48 +1,67 @@
-
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 class Tree {
-    root;
+  root;
 
-    buildTree(array) {
-        array = this.sortArray(array);
-        return this.sortedArrayToBst(array, 0, array.length - 1); 
+  buildTree(array) {
+    array = this.sortArray(array);
+    this.root = this.sortedArrayToBst(array, 0, array.length - 1);
+  }
+
+  sortArray(array) {
+    let sortedArray = array.sort((a, b) => a - b);
+
+    for (let i = 0; i < array.length; i++)
+      if (array[i] === array[i + 1]) {
+        array.splice(i, 1);
+      }
+
+    return sortedArray;
+  }
+
+  sortedArrayToBst(arr, start, end) {
+    if (start > end) {
+      return;
     }
 
-    sortArray(array) {
-        let sortedArray = array.sort((a, b) => a - b);
+    let mid = start + Math.floor((end - start) / 2);
 
-        for (let i = 0; i < array.length; i++)
-            if(array[i] === array[i + 1]) {
-                array.splice(i, 1);
-            }
+    let root = new Node(arr[mid]);
+    root.left = this.sortedArrayToBst(arr, start, mid - 1);
+    root.right = this.sortedArrayToBst(arr, mid + 1, end);
 
-        return sortedArray;
+    return root;
+  }
+
+  insert(root, key) {
+    if (root === undefined) {
+        return new Node(key);
     }
 
-    sortedArrayToBst(arr, start, end){
-        if (start > end) {
-            return
-        }
-
-        let mid = start + Math.floor((end - start) / 2);
-
-        let root = new Node(arr[mid]);
-        root.left = this.sortedArrayToBst(arr, start, mid -1);
-        root.right = this.sortedArrayToBst(arr, mid + 1, end);
-
-        return root
-
+    if (root.data === key) {
+        return root;
     }
 
+    if (key < root.data) {
+        root.left = this.insert(root.left, key);
+    }
+    else if (key > root.data) {
+        root.right = this.insert(root.right, key);
+    }
+
+    return root;
+  }
 }
 
 const newTree = new Tree();
 
-console.log(newTree.buildTree([4, 3, 2, 1, 4]));
+newTree.buildTree([40, 30, 20, 10]);
+newTree.insert(newTree.root, 25)
+
+console.log(newTree.root);
